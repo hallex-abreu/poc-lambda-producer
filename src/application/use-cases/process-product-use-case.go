73dd31347f733cc66241f22bcbc0aed120b0e857
Hallex Abreu <hallexabreu@MacBook-Air-de-Hallex.local>
2023-02-18 18:23:50 -0300

    feat: finished sending sqs

--- src/application/use-cases/process-product-use-case.go
+++ src/application/use-cases/process-product-use-case.go
@@ -1,10 +1,12 @@
 package usecases
 
 import (
+	"encoding/json"
 	"errors"
 	"github/hallex-abreu/poc-lambda-producer/src/application/adapters"
 	"github/hallex-abreu/poc-lambda-producer/src/application/repositories"
 	"github/hallex-abreu/poc-lambda-producer/src/domain"
+	"strconv"
 )
 
 func ProcessProductUseCase(productRepository repositories.ProductRepository, queueAdapter adapters.QueueAdapter, logAdapter adapters.LogAdapter) error {
@@ -16,7 +18,12 @@ func ProcessProductUseCase(productRepository repositories.ProductRepository, que
 		return errors.New(err.Error())
 	}
 
-	logAdapter.Log("info", "Finalizado busca de produtos")
+	if len(products) == 0 {
+		logAdapter.Log("info", "Sem novos produtos para integrar")
+		return nil
+	}
+
+	logAdapter.Log("info", "Finalizado busca de produtos com total de: "+strconv.Itoa(len(products)))
 
 	for _, product := range products {
 		product_format := &domain.Product{
@@ -27,9 +34,18 @@ func ProcessProductUseCase(productRepository repositories.ProductRepository, que
 
 		logAdapter.Log("info", "Iniciando envio de produto para fila")
 		queueAdapter.SendingInSqs(product_format)
-		logAdapter.Log("info", "finalizado envio de produto para fila")
+		product_log, _ := json.Marshal(product)
+		logAdapter.Log("info", "Finalizado envio de produto para fila: "+string(product_log))
+		logAdapter.Log("info", "Iniciando processo de integração")
+		err := productRepository.ProcessIntegrationProduct(product)
+
+		if err != nil {
+			logAdapter.Log("error", err.Error())
+			return errors.New(err.Error())
+		}
+
+		logAdapter.Log("info", "Finalizando processo de integração")
 	}
 
-	logAdapter.Log("info", "finalizado processamento com sucesso")
 	return nil
 }
